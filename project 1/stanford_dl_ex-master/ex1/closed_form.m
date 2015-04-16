be874e8064b0558fa8ab8d0a730dced37f9e9bfd
Bobak Hashemi <bth5032@gmail.com>
2015-04-15 20:57:50 -0700

    cleaned up unnecessary lines in linear_regression.m and closed_form.m

--- project 1/stanford_dl_ex-master/ex1/closed_form.m
+++ project 1/stanford_dl_ex-master/ex1/closed_form.m
@@ -2,7 +2,4 @@ function [theta, f, g, exitflag ] = closed_form( X, y )
 %CLOSED_FORM Summary of this function goes here
 %   Detailed explanation goes here
 theta=(X*X')\((X)*(y'));
-[f, g]=linear_regression(theta, X, y); 
-exitflag=1;
 end
-
