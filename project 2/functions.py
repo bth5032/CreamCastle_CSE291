561f92ba8c066cccc46a901232048fce0131b066
Liam <liam.fedus@gmail.com>
2015-04-20 13:16:47 -0700

    Creating initial files for code

---
+++ project 2/functions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def preprocess_data():
+'''Preprocessing code
+   0.  Extract 0 and 1 digits only from Test/Training
+   1.  Append '1' feature to dataset for intercept term'''
+	pass
+
+def gradient_descent():
+	pass
+
+def stochastic_gradient_descent():
+	pass
