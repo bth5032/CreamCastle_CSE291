585f693a999d58f93602668adee863a4b242a8a2
Liam <liam.fedus@gmail.com>
2015-04-21 10:55:12 -0700

    Normalize pixel intensities

--- project 2/helper_functions.py
+++ project 2/helper_functions.py
@@ -11,14 +11,18 @@ def display_image(dataset, labels, index):
 
 def preprocess_data(dataset, labels):
 	''' Preprocessing code
-	0.  Extract 0 and 1 digits only from Test/Training
-	1.  Append '1' feature to dataset for intercept term'''
+	0.  Normalize the pixel intensities to zero mean, unit variance
+	1.  Extract 0 and 1 digits only from Test/Training
+	2.  Append '1' feature to dataset for intercept term'''
 	X_list = []
 	Y_list = []
 
 	for i in range(0, len(dataset)):
 		if labels[i] == 0 or labels[i] == 1: 
-			X_list.append([np.append(1.0, dataset[i])])
+			mean = dataset[i].mean()
+			std  = dataset[i].std()
+
+			X_list.append([np.append(1.0, (dataset[i] - mean)/std)])
 			Y_list.append(labels[i])
 
 	X = np.asarray(X_list)
