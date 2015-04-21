696192f57e1d0db20ba1f3c15dfbf73d66d865c2
Liam <liam.fedus@gmail.com>
2015-04-21 11:13:03 -0700

    Adding sklearn logistic regression

--- project 2/softmaxregression.py
+++ project 2/softmaxregression.py
@@ -1,10 +1,8 @@
 '''SoftMax Regression for Digits in MNIST dataset'''
 import numpy as np
-
-if __name__ == "__main__":
 	
-	# Load dataset from MNIST
-	full_trainarray = np.load('data/numpy/trainarray.npy')
-	full_trainlabel = np.load('data/numpy/trainlabel.npy')
-	full_testarray  = np.load('data/numpy/testarray.npy' )
-	full_testlabel  = np.load('data/numpy/testlabel.npy' )
+# Load dataset from MNIST
+full_trainarray = np.load('data/numpy/trainarray.npy')
+full_trainlabel = np.load('data/numpy/trainlabel.npy')
+full_testarray  = np.load('data/numpy/testarray.npy' )
+full_testlabel  = np.load('data/numpy/testlabel.npy' )
