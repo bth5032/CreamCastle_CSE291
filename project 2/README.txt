bc1ff4070846af44d68e79f23724ee2eadc1cbf8
Liam <liam.fedus@gmail.com>
2015-04-21 09:48:38 -0700

    README and renmaing

--- project 2/README.txt
+++ project 2/README.txt
@@ -1,1 +1,14 @@
-Testing
+# Description
+This has two main functions:
+* Logisitic regresssion for the classification of handwritten '0' and '1'
+* Softmax regression for the classification of handwritten digits {'0',...,'9'}
+
+# Dataset
+The data comes from the MNIST dataset of handwritten digits.  Data has been processed from IDX format into Numpy format
+
+# Code Base
+There are two basic functions, logisiticregression.py and softmaxregression.py
+
+Helper code for preprocessing, gradient descent is found in helper_functions.py
+
+Mathematical expressions are found in math_functions.py
--- project 2/math_functions.py
+++ project 2/math_functions.py
@@ -1,21 +1,25 @@
 import numpy as np
 
 
-def probability(x, y, w):
+def sigmoid_probability(x, y, w):
 	'''Probability of class y given x.  Sigmoid function'''
 	return 1.0/(1.0 + np.exp(- y * np.dot(w,x) ))
 
-def loss(X, Y, w):
-	'''Loss Function'''
+def sigmoid_loss(X, Y, w):
+	'''Sigmoid Loss Function'''
 	val = 0.0
 	for i in range(0, len(X)):
 		val += np.log(1.0 + np.exp(-Y[i]*np.dot(w,X[i])))
 	return val
 
-def gradient(X, Y, w):
-	'''Gradient of Loss Function'''	
+def sigmoid_gradient(X, Y, w):
+	'''Gradient of Sigmoid Loss Function'''	
 	pass
 
-def stochastic_gradient(X, Y, w):
-	'''Gradient of Loss Function'''
+def gradient_descent(X, Y, w):
+	'''Gradient descent of Loss Function'''
+	pass
+
+def stochastic_gradient_descent(X, Y, w):
+	'''Stochastic gradient descent of Loss Function'''
 	pass
