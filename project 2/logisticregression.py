ca2acda4b71688a022a03146839ceb80fdab3045
Bobak Hashemi <bth5032@gmail.com>
2015-04-21 20:22:09 -0700

    fixed conflict removing if/main arg in logistic_regression

--- project 2/logisticregression.py
+++ project 2/logisticregression.py
@@ -1,7 +1,7 @@
 '''Logistic Regression for 0/1 in MNIST dataset'''
 import numpy as np
 import helper_functions as fn
-	
+
 # Load dataset from MNIST
 full_trainarray = np.load('data/numpy/trainarray.npy')
 full_trainlabel = np.load('data/numpy/trainlabel.npy')
@@ -13,8 +13,3 @@ X_test, Y_test = fn.preprocess_data(full_testarray, full_testlabel)
 
 # Logistic regression via sklearn
 #w = fn.logistic_regression_package(X, Y, regularization = 1.0)
-
-
-	
-
-
