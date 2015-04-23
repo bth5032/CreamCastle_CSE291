8521e7801eea30252f2e9eadfd154ca3018d70f4
Liam <liam.fedus@gmail.com>
2015-04-22 19:22:11 -0700

    Working on gradient descent, overflow issue

--- project 2/math_functions.py
+++ project 2/math_functions.py
@@ -1,3 +1,4 @@
+import bigfloat
 import numpy as np
 
 
@@ -9,16 +10,54 @@ def sigmoid_loss(X, Y, w):
 	'''Sigmoid Loss Function'''	
 	val = 0.0
 	for i in range(0, len(X)):
-		val += np.log(1.0 + np.exp(-Y[i]*np.dot(w,X[i])))
+		val += bigfloat.log(1.0 + bigfloat.exp(-Y[i]*np.dot(w,X[i])))
 	return val
 
 def sigmoid_gradient(X, Y, w):
-	'''Gradient of Sigmoid Loss Function'''	
-	pass
+	'''Gradient of Sigmoid Loss Function
+	Input:
+		0.  Training Examples Matrix, X.  
+		1.  Training Labels Vector,   Y
+		2.  Initalized Weight Vector, w
+	Output:
+		Gradient of loss function at w'''	
+	val = np.zeros(len(w))
 
-def gradient_descent(X, Y, w):
-	'''Gradient descent of Loss Function'''
-	pass
+	for i in range(0, len(w)):
+		val += -X[i]*Y[i]/(1.0 + bigfloat.exp(Y[i] * np.dot(w, X[i])))
+	return val
+
+
+def gradient_descent(X, Y, w, M):
+	'''Gradient descent of Loss Function using backtracking
+	Input: 
+		0.  Training Examples Matrix, X.  
+		1.  Training Labels Vector,   Y
+		2.  Initalized Weight Vector, w
+		3.  Max Number of Iterations, M
+	Output:
+		Optimized Weight Vector,      w
+	Further information:  
+	# Backtracking:  http://users.ece.utexas.edu/~cmcaram/EE381V_2012F/Lecture_4_Scribe_Notes.final.pdf	'''
+	
+	alpha = 0.15
+	beta  = 0.5
+	
+	for i in range(0, M):
+		eta = 1.0
+
+		grad = sigmoid_gradient(X,Y,w)
+
+		while sigmoid_loss(X, Y, (w - eta * grad)) >= (sigmoid_loss(X,Y,w) - alpha * eta *np.linalg.norm(grad)):
+			eta = beta * eta
+
+			if eta < 10E-5:
+				break
+
+		w = w - eta * sigmoid_loss(X, Y, w)
+		print ("Log-loss: ", sigmoid_loss(X,Y,w), " at i = ", i)
+	return w
+
 
 def stochastic_gradient_descent(X, Y, w):
 	'''Stochastic gradient descent of Loss Function'''
--- project 2/test.py
+++ project 2/test.py
@@ -1,7 +1,9 @@
 '''Logistic Regression for 0/1 in MNIST dataset'''
 import numpy as np
 import helper_functions as fn
+import math_functions as mf
 from logisticregression import LogisticRegression
+
 	
 # Load dataset from MNIST
 full_trainarray = np.load('data/numpy/trainarray.npy')
@@ -12,6 +14,7 @@ full_testlabel  = np.load('data/numpy/testlabel.npy' )
 X_train, Y_train = fn.preprocess_data(full_trainarray, full_trainlabel)
 X_test, Y_test   = fn.preprocess_data(full_testarray, full_testlabel)
 
+'''
 # Logistic Regression using sklearn
 logreg = LogisticRegression(X_train, Y_train, X_test, Y_test)
 logreg.fit()
@@ -19,6 +22,11 @@ logreg.fit()
 print logreg.w
 predict = logreg.predict()
 print logreg.incorrect
+'''
+w = np.zeros(X_train.shape[1])
+w = mf.gradient_descent(X_train, Y_train, w, 1000)
+
+print w
 
 
 
