225049e5d53366cc26290829b06c22ba518cae6b
Liam <liam.fedus@gmail.com>
2015-04-27 20:38:00 -0700

    Add in timing

--- project 2/run_softmaxregression.py
+++ project 2/run_softmaxregression.py
@@ -5,6 +5,7 @@ import math_softmax as ms
 from softmaxregression import SklearnSoftmaxRegression
 from logisticregression import SklearnLogisticRegression
 from sklearn import linear_model
+import time
 
 #--------------------#
 # Softmax Regression #
@@ -18,16 +19,22 @@ full_testlabel  = np.load('data/numpy/testlabel.npy' )
 X_train, Y_train = fn.preprocess_data(full_trainarray, full_trainlabel, False)
 X_test, Y_test   = fn.preprocess_data(full_testarray, full_testlabel, False)
 
-'''
+
 # 0.  Sklearn softmax regression
 print 'Softmax regression using sklearn'
+t0 = time.time()
 softmax = SklearnSoftmaxRegression(tolerance = 0.1)
 W = softmax.train(X_train, Y_train)
 p = softmax.predict(X_test)
+t1 = time.time()
+print ' Time:', t1-t0
 fn.print_performance(p, Y_test)
-'''
+
 
 # 0.  Softmax regression using stochastic gradient descent
 print 'Softmax regression using stochastic gradient descent'
+t0 = time.time()
 W = np.zeros([X_train.shape[1], 10])
 W = ms.stochastic_gradient_descent(X_train, Y_train, W, 10000, 100)
+t1 = time.time()
+print ' Time:', t1-t0
