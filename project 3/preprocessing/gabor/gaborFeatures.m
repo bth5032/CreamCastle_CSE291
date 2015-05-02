d067cbe8c278040d5a2ba0e44717a293d80f1d0d
Liam <liam.fedus@gmail.com>
2015-05-01 20:55:45 -0700

    Gabor

--- project 3/preprocessing/gabor/gaborFeatures.m
+++ project 3/preprocessing/gabor/gaborFeatures.m
@@ -91,21 +91,21 @@ end
 
 %% Show filtered images
 
-% % Show real parts of Gabor-filtered images
-% figure('NumberTitle','Off','Name','Real parts of Gabor filters');
-% for i = 1:u
-%     for j = 1:v        
-%         subplot(u,v,(i-1)*v+j)    
-%         imshow(real(gaborResult{i,j}),[]);
-%     end
-% end
-% 
-% 
-% % Show magnitudes of Gabor-filtered images
-% figure('NumberTitle','Off','Name','Magnitudes of Gabor filters');
-% for i = 1:u
-%     for j = 1:v        
-%         subplot(u,v,(i-1)*v+j)    
-%         imshow(abs(gaborResult{i,j}),[]);
-%     end
-% end
+% Show real parts of Gabor-filtered images
+figure('NumberTitle','Off','Name','Real parts of Gabor filters');
+for i = 1:u
+    for j = 1:v        
+        subplot(u,v,(i-1)*v+j)    
+        imshow(real(gaborResult{i,j}),[]);
+    end
+end
+
+
+% Show magnitudes of Gabor-filtered images
+figure('NumberTitle','Off','Name','Magnitudes of Gabor filters');
+for i = 1:u
+    for j = 1:v        
+        subplot(u,v,(i-1)*v+j)    
+        imshow(abs(gaborResult{i,j}),[]);
+    end
+end
