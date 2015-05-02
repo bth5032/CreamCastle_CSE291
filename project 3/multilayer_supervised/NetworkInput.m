123ce5b8743a8c2e16d9f7393ee78363f6f9e2d4
mattb243 <mattb243@gmail.com>
2015-05-01 21:30:00 -0700

    Create network classes

---
+++ project 3/multilayer_supervised/NetworkInput.m
@@ -0,0 +1,25 @@
+classdef NetworkInput
+    %NETWORKINPUT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        ei
+        data
+        
+        %This will be the multiscale 
+        convolved_features
+    end
+    
+    methods
+        function obj = NetworkInput(ei, data)
+                
+        end
+        
+        function gaborFilter(obj)
+        
+        end
+        
+    end
+    
+end
+
---
+++ project 3/multilayer_supervised/NetworkOutput.m
@@ -0,0 +1,29 @@
+classdef NetworkOutput
+    %NETWORKOUTPUT 
+    
+    properties
+        gradient
+        log_liklihood
+        loss
+        weights
+    end
+    
+    methods
+        %Plot the convergence of the Network 
+        function h = plot_convergence(obj)
+        end
+        
+        %Plot the convergence of the Network 
+        function h = log_liklihood(obj)
+        end
+
+        %Plot the convergence of the Network 
+        function h = loss(obj)
+        end
+        
+        %Plot the convergence of the Network 
+        function h = weights(obj)
+        end
+    end     
+end
+
