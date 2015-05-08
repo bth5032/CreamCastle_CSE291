99962d89f807878a67b5dd1ea6ce5b8ccef4beb5
mattb243 <mattb243@gmail.com>
2015-05-08 16:00:33 -0700

    Network constructor complete

--- project 3/multilayer_supervised/Network.m
+++ project 3/multilayer_supervised/Network.m
@@ -10,19 +10,30 @@ classdef Network < matlab.mixin.Copyable
     end
     
     methods
+        
         %Constructor: takes NetworkDesign and NetworkInput ojbects as
         %arguments.
         function obj = Network(input, design)
             %Check input
             assert(isa(design, 'NetworkDesign'), 'Network (constructor): design must be of NetworkDesign class');
             assert(isa(input, 'NetworkInput'), 'Network (constructor): design must be of NetworkDesign class');
+            assert(length(input)==length(design),'Network (constructor): input and design must be same length'); 
             
-            for i=1:length(design)
-                for j=1:length(input)
-                    obj(i,j).network_design=design(i);
-                    obj(i,j).network_input=input(i);
-                    obj(i,j).network_output = NetworkOutput;
-                    obj(i,j).fwd_prop; %Updates obj(i).output
+            %Create a single network for each input/design pair 
+            if length(input)==1 && length(design)==1
+                obj.network_design=design;
+                obj.network_input=input;
+                obj.network_output = NetworkOutput;
+                obj.fwd_prop; %Updates obj(i).output
+                return;
+                
+            %We are passed a vector of inputs/designs: recursively call
+            %for each pair. 
+            else
+                n=1; 
+                for i=1:length(design)
+                    obj(n) = Network(input(i), design(i));
+                    n=n+1; 
                 end
             end
         end
@@ -31,20 +42,23 @@ classdef Network < matlab.mixin.Copyable
         function stepLearnWeights(obj, num_iterations)
         end
         
-        %Make one gradient descent step in this network
+        %Compute current outputs at all layers
         function fwd_prop(obj)
-            %Compute current outputs
+            return;
         end
         
         %Perform backprop on network
-        function gradient_desc(obj)
+        function gradientDesc(obj)
+            return;
         end
         
         %2.b - Check gradient using numerical approximations (unit test for gradient)
         function checkGradient(obj)
+            return;
         end
         
         function loss(obj)
+            return;
         end
         
     end
