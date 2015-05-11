105b700b713569de251424498921f54b218fe9fd
mattb243 <mattb243@gmail.com>
2015-05-11 14:46:52 -0700

    Refining Network class

--- project 3/multilayer_supervised/Network.m
+++ project 3/multilayer_supervised/Network.m
@@ -3,61 +3,73 @@ classdef Network < matlab.mixin.Copyable
     
     properties
         network_design
-        train_input
-        test_input
-        
+        network_input
         network_output
-        network_converged=0
+    end
+    
+    properties(Constant)
+        MAXITER=1000;
     end
     
     methods
         
         %Constructor: takes NetworkDesign and NetworkInput ojbects as
         %arguments.
-        function obj = Network(design, train_input, test_input)
+        function obj = Network(design, full_data)
             %Check input
             assert(isa(design, 'NetworkDesign'), 'Network (constructor): design must be of NetworkDesign class');
             assert(isa(input, 'NetworkInput'), 'Network (constructor): design must be of NetworkDesign class');
-            assert(length(input)==length(design),'Network (constructor): input and design must be same length'); 
+            assert(length(input)==length(design),'Network (constructor): input and design must be same length');
             
-            %Create a single network for each input/design pair 
+            %Create a single network for each input/design pair
             if length(input)==1 && length(design)==1
                 obj.network_design=design;
                 obj.train_input=train_input;
                 obj.test_input=test_input;
-                obj.network_output = NetworkOutput;
+                obj.network_output = NetworkOutput(obj);
                 return;
                 
-            %We are passed a vector of inputs/designs: recursively call
-            %for each pair. 
+                %We are passed a vector of inputs/designs: recursively call
+                %for each pair.
             else
-                n=1; 
+                n=1;
                 for i=1:length(design)
                     obj(n) = Network(design(i), train_input(i), test_input(i));
-                    n=n+1; 
+                    n=n+1;
                 end
             end
         end
         
         %Train the network on train_input
         function train(obj)
+            for i=1:length(obj) 
+                %Iterate until MAXITER or converged
+                n=1; 
+                while obj(i).checkConvergence || (n > obj.MAXITER)
+                    fprintf('Network.train: training Network %d/%d\n', i, length(obj));
+                    folds = obj(i).network_input.makeXvalFolds;
+                    obj(i).forwardProp;
+                    obj(i).backProp(folds);
+                    n=n+1; 
+                end
+            end
         end
         
         %Test the network on test_input
         function test(obj)
         end
         
-        %Compute all network activations 
+        %Compute all network activations
         function forwardProp(obj)
-            obj.network_design.ei.inputdim
+            obj.network_design.ei.inputdim;
         end
-
+        
+        function backProp(obj)
+        end
+        
     end
     
     methods(Hidden=true)
-        %Make one gradient descent step in this network
-        function stepLearnWeights(obj, num_iterations)
-        end
         
         %Perform backprop on network
         function gradientDesc(obj)
@@ -66,6 +78,10 @@ classdef Network < matlab.mixin.Copyable
         
         %2.b - Check gradient using numerical approximations (unit test for gradient)
         function checkGradient(obj)
+            return;
+        end
+        
+        function checkConverged(obj)
             return;
         end
         
